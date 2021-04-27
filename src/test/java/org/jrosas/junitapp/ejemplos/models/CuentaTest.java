b6261f834768b84b61b5485d21009cafb2e43213
juan <juan.antoniorr@hotmail.com>
2021-04-26 20:09:11 -0500

    adding AssertEquals

--- src/test/java/org/jrosas/junitapp/ejemplos/models/CuentaTest.java
+++ src/test/java/org/jrosas/junitapp/ejemplos/models/CuentaTest.java
@@ -10,42 +10,41 @@ class CuentaTest {
 
 	@Test
 	void testNombreCuenta() {
-		Cuenta cuenta = new Cuenta("Juan", new BigDecimal("123.456") );
+		Cuenta cuenta = new Cuenta("Juan", new BigDecimal("123.456"));
 		String esperado = "Juan";
 		String real = cuenta.getPersona();
 		assertEquals(esperado, real);
-		
+
 	}
-	
-	@Test 
+
+	@Test
 	void testSaldoCuenta() {
-		Cuenta cuenta = new Cuenta("Juan", new BigDecimal("123.456") );
+		Cuenta cuenta = new Cuenta("Juan", new BigDecimal("123.456"));
 		assertEquals(123.456, cuenta.getSaldo().doubleValue());
 		assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
 	}
-	
+
 	@Test
 	void testReferenciaCuenta() {
 		Cuenta cuenta = new Cuenta("John", new BigDecimal("1000.50"));
 		Cuenta cuenta2 = new Cuenta("John", new BigDecimal("1000.50"));
-		//Pasa porque sobreescribi el metodo equals y compare por valor
+		// Pasa porque sobreescribi el metodo equals y compare por valor
 		assertEquals(cuenta, cuenta2);
 	}
-	
+
 	@Test
 	void testDineroInsuficienteException() {
 		Cuenta cuenta = new Cuenta("John", new BigDecimal("1000.50"));
-		
+
 		Exception ex = assertThrows(DineroInsuficienteException.class, () -> {
 			cuenta.debito(new BigDecimal(1500));
-			
-			
+
 		});
 		String actual = ex.getMessage();
 		String esperado = "Dinero Insuficiente";
 		assertEquals(esperado, actual);
 	}
-	
+
 	@Test
 	void testTransferir() {
 		Cuenta cuenta1 = new Cuenta("John", new BigDecimal("10000"));
@@ -55,9 +54,9 @@ class CuentaTest {
 		banco.transferir(cuenta1, cuenta2, new BigDecimal(500));
 		assertEquals("9500", cuenta1.getSaldo().toPlainString());
 		assertEquals("1500", cuenta2.getSaldo().toPlainString());
-		
+
 	}
-	
+
 	@Test
 	void testRelacionBancoCuentas() {
 		Cuenta cuenta1 = new Cuenta("John", new BigDecimal("10000"));
@@ -65,19 +64,26 @@ class CuentaTest {
 		Banco banco = new Banco();
 		banco.addCuenta(cuenta1);
 		banco.addCuenta(cuenta2);
-		
+
 		banco.setNombre("Bancomer");
 		banco.transferir(cuenta1, cuenta2, new BigDecimal(500));
 		assertEquals("9500", cuenta1.getSaldo().toPlainString());
 		assertEquals("1500", cuenta2.getSaldo().toPlainString());
-		assertEquals(2, banco.getCuentas().size());
-		//Tenemos relacion bidireccional 
-		assertEquals("Bancomer", cuenta1.getBanco().getNombre());
-		//find first operacion final, get obtiene el elemento.
-		//Metodo que busca si el nombre esta en el registro del banco
-		assertEquals("Saul", banco.getCuentas().stream()
-				.filter((p) -> p.getPersona().equals("Saul")).
-				findFirst().get().getPersona());
+		assertAll(() -> {
+			assertEquals(2, banco.getCuentas().size());
+		},
+				// Tenemos relacion bidireccional
+				() -> {
+					assertEquals("Bancomer", cuenta1.getBanco().getNombre());
+				}, () -> {
+					// find first operacion final, get obtiene el elemento.
+					// Metodo que busca si el nombre esta en el registro del banco
+					assertEquals("Saul", banco.getCuentas().stream().filter((p) -> p.getPersona().equals("Saul"))
+							.findFirst().get().getPersona());
+				});
+
+		
+
 	}
-	
+
 }
